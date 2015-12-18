21f71bda8f7aa0ad7346007694a9423148157efd
Roman Hoog Antink <rha_github@disconnect.ch>
2015-12-18 18:34:28 +0100

    DerChaot: increase efficiency

--- src/sort/DerChaot.java
+++ src/sort/DerChaot.java
@@ -13,28 +13,33 @@ public class DerChaot extends Sortiermethode {
     @Override
     public void sortierSchritt() {
         super.sortierSchritt();
-        
-        // suche zwei zufällige Positionen aus
-        int j1 = (int)(Math.random() * zahlenreihe.size());
-        int j2 = (int)(Math.random() * zahlenreihe.size());
 
-        if(j1 == j2) {
-            if(j1 < zahlenreihe.size()-1)
-                j1++;
-            else
-                j1--;
+        int c = 0;
+        while(++c < 100) {
+            // suche zwei zufällige Positionen aus
+            int j1 = (int) (Math.random() * zahlenreihe.size());
+            int j2 = (int) (Math.random() * zahlenreihe.size());
+
+            if (j1 == j2) {
+                if (j1 < zahlenreihe.size() - 1)
+                    j1++;
+                else
+                    j1--;
+            }
+
+            if (j1 > j2) {
+                int y = j2;
+                j2 = j1;
+                j1 = y;
+            }
+
+            int z1 = zahlenreihe.get(j1);
+            int z2 = zahlenreihe.get(j2);
+
+            if (z1 > z2) {
+                vertausche(j1, j2);
+                break;
+            }
         }
-
-        if(j1 > j2) {
-            int y = j2;
-            j2 = j1;
-            j1 = y;
-        }
-
-        int z1 = zahlenreihe.get(j1);
-        int z2 = zahlenreihe.get(j2);
-
-        if(z1 > z2)
-            vertausche(j1, j2);
     }
 }
